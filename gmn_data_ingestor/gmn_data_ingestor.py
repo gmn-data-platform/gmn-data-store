475ced05e60550998493da560a62f2ed7142a197
Ricky Bassom <rickybas12@gmail.com>
2022-03-08 23:40:35 +0000

    Add historical ingestion

--- gmn_data_ingestor/gmn_data_ingestor.py
+++ gmn_data_ingestor/gmn_data_ingestor.py
@@ -10,7 +10,8 @@ from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 
 from gmn_python_api.data_directory import DATA_START_DATE
-from gmn_python_api.data_directory import get_daily_file_content_by_date
+from gmn_python_api.data_directory import get_daily_file_content_by_date, \
+    get_file_content_from_url
 from gmn_python_api.trajectory_summary_reader import \
     read_trajectory_summary_as_dataframe, DATETIME_FORMAT
 
@@ -19,9 +20,11 @@ AVRO_SCHEMA_PATH = f"{os.path.dirname(__file__)}/avro/trajectory_summary_schema.
 AVRO_SCHEMA = avro.load(AVRO_SCHEMA_PATH)
 
 
-def save_extracted_data_file(extracted_data_directory: str, target_date: datetime,
-                             logical_date: datetime,
-                             execution_date: datetime, file_content: str) -> str:
+def save_extracted_daily_trajectory_summary_file(extracted_data_directory: str,
+                                                 target_date: datetime,
+                                                 logical_date: datetime,
+                                                 execution_date: datetime,
+                                                 file_content: str) -> str:
     execution_date_extracted_data_directory = os.path.join(
         os.path.expanduser(extracted_data_directory),
         execution_date.strftime('%Y%m%d'))
@@ -36,29 +39,54 @@ def save_extracted_data_file(extracted_data_directory: str, target_date: datetim
     return file_path
 
 
+def save_all_trajectory_summary_file(extracted_data_directory: str,
+                                     execution_date: datetime,
+                                     file_content: str) -> str:
+    execution_date_extracted_data_directory = os.path.join(
+        os.path.expanduser(extracted_data_directory),
+        execution_date.strftime('%Y%m%d') + "_historical")
+    os.makedirs(execution_date_extracted_data_directory, exist_ok=True)
+    filename = f"execution_{execution_date.strftime('%Y%m%d')}_historical.log"
+    file_path = os.path.join(execution_date_extracted_data_directory, filename)
+    file = open(file_path, "w")
+    file.write(file_content)
+    file.close()
+
+    return file_path
+
+
+def delivery_trajectory_summary(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+
 def gmn_data_to_kafka_daily(day_offset: int = 0):
     print(day_offset)
     context = get_current_context()
     target_date = context['logical_date'] + timedelta(days=day_offset)
     print(
-        f"Extracting data from {target_date} in dag run with logical date "
+        f"Extracting daily data from {target_date} in dag run with logical date "
         f"{context['logical_date']} "
-        f"on current execution date {context['execution_date']}")
+        f"on current execution date {context['execution_date']}"
+        f" day_offset {day_offset}")
 
     # Find target_date filename by looking at the directory listing
     file_content = get_daily_file_content_by_date(target_date,
                                                   context['execution_date'])
 
     # Write file to ~/extracted_date/{execution_date}/
-    extracted_file_path = save_extracted_data_file(EXTRACTED_DATA_DIRECTORY,
-                                                   target_date, context['logical_date'],
-                                                   context['execution_date'],
-                                                   file_content)
+    extracted_file_path = save_extracted_daily_trajectory_summary_file(
+        EXTRACTED_DATA_DIRECTORY,
+        target_date, context['logical_date'],
+        context['execution_date'],
+        file_content)
 
     trajectory_df = read_trajectory_summary_as_dataframe(extracted_file_path)
     print(f"Shape of the data = {trajectory_df.shape}\n")
-    print("First 5 rows of the DataFrame: ")
-    print(trajectory_df.info())
 
     trajectory_df['IAU (code)'] = trajectory_df['IAU (code)'].astype('unicode')
     trajectory_df['Participating (stations)'] = trajectory_df[
@@ -72,26 +100,62 @@ def gmn_data_to_kafka_daily(day_offset: int = 0):
     trajectory_df.columns = trajectory_df.columns.str.replace('Q_AU', 'q_au_')
     trajectory_df.columns = trajectory_df.columns.str.lower()
 
-    def delivery_report(err, msg):
-        """ Called once for each message produced to indicate delivery result.
-            Triggered by poll() or flush(). """
-        if err is not None:
-            print('Message delivery failed: {}'.format(err))
-        else:
-            print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
-
     avroProducer = AvroProducer({
         'bootstrap.servers': 'kafka-broker:29092',
-        'on_delivery': delivery_report,
+        'on_delivery': delivery_trajectory_summary,
         'schema.registry.url': 'http://schema-registry:8081'
     }, default_key_schema=None, default_value_schema=AVRO_SCHEMA)
 
     for index, row in trajectory_df.iterrows():
+        print(f"Sending index {index}, row = {row} to kafka")
         avroProducer.produce(topic='extracted', value=row.to_dict(), key=None)
+        avroProducer.poll(0)
+        print(f"Successfully sent index {index}")
     avroProducer.flush()
 
 
 def gmn_data_to_kafka_historical():
+    context = get_current_context()
+    print(
+        f"Historically extracting data from traj_summary_all.txt "
+        f"execution date {context['execution_date']}")
+
+    # Find target_date filename by looking at the directory listing
+    file_content = get_file_content_from_url("https://globalmeteornetwork.org/data/traj_summary_data/traj_summary_all.txt")
+
+    # Write file to ~/extracted_date/{execution_date}/
+    extracted_file_path = save_all_trajectory_summary_file(
+        EXTRACTED_DATA_DIRECTORY,
+        context['execution_date'],
+        file_content)
+
+    trajectory_df = read_trajectory_summary_as_dataframe(extracted_file_path)
+    print(f"Shape of the data = {trajectory_df.shape}\n")
+
+    trajectory_df['IAU (code)'] = trajectory_df['IAU (code)'].astype('unicode')
+    trajectory_df['Participating (stations)'] = trajectory_df[
+        'Participating (stations)'].astype('unicode')
+    trajectory_df['Beginning (UTC Time)'] = trajectory_df[
+        'Beginning (UTC Time)'].dt.strftime(DATETIME_FORMAT)
+
+    trajectory_df.columns = trajectory_df.columns.str.replace('[^0-9a-zA-Z]+', '_')
+    trajectory_df.columns = trajectory_df.columns.str.rstrip('_')
+    trajectory_df.columns = trajectory_df.columns.str.lstrip('_')
+    trajectory_df.columns = trajectory_df.columns.str.replace('Q_AU', 'q_au_')
+    trajectory_df.columns = trajectory_df.columns.str.lower()
+
+    avroProducer = AvroProducer({
+        'bootstrap.servers': 'kafka-broker:29092',
+        'on_delivery': delivery_trajectory_summary,
+        'schema.registry.url': 'http://schema-registry:8081'
+    }, default_key_schema=None, default_value_schema=AVRO_SCHEMA)
+
+    for index, row in trajectory_df.iterrows():
+        print(f"Sending index {index}, row = {row.to_dict()} to kafka")
+        avroProducer.produce(topic='extracted', value=row.to_dict(), key=None)
+        avroProducer.poll(0)
+        print(f"Successfully sent index {index}")
+    avroProducer.flush()
 
 
 default_args = {
