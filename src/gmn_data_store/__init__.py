932d4c300f2bdd5859be25ab1fdb1fe8b6fb3145
Ricky Bassom <rickybas12@gmail.com>
2022-04-19 01:30:00 +0100

    Use WAL mode for concurrent access

--- src/gmn_data_store/__init__.py
+++ src/gmn_data_store/__init__.py
@@ -1,10 +1,13 @@
 """GMN Data Store."""
 from pathlib import Path
+from sqlite3 import Connection
 
 from sqlalchemy import create_engine  # type: ignore
+from sqlalchemy import event
 from sqlalchemy.engine import Engine  # type: ignore
 from sqlalchemy.orm import Session  # type: ignore
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import _ConnectionRecord  # type: ignore
 
 _DB_DIRECTORY = f"{str(Path.home())}/.gmn_data_store/gmn_data_store.db"
 _DB_CONNECTION_URI = f"sqlite:///{_DB_DIRECTORY}"
@@ -15,7 +18,9 @@ def get_engine() -> Engine:
     Create an engine for the database.
     :return: The engine for the database.
     """
-    return create_engine(_DB_CONNECTION_URI)
+    engine = create_engine(_DB_CONNECTION_URI)
+    event.listen(engine, "connect", _pragma_on_connect)
+    return engine
 
 
 def get_session(engine: Engine) -> Session:
@@ -24,3 +29,17 @@ def get_session(engine: Engine) -> Session:
     :return: A session for the database.
     """
     return sessionmaker(bind=engine)()
+
+
+def _pragma_on_connect(
+    dbapi_connection: Connection, connection_record: _ConnectionRecord
+) -> None:
+    """
+    Enable Write-Ahead Logging for concurrent access.
+    :param dbapi_connection: The database connection.
+    :param connection_record: The connection record.
+    :return: None.
+    """
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode = WAL")
+    cursor.close()
